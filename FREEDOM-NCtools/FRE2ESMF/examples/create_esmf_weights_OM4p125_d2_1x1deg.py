c2410e18e81654923dfb7d296709398965773098
Raphael Dussin <Raphael.Dussin@noaa.gov>
2020-10-01 16:21:05 -0400

    add esmf2fre+example

---
+++ FREEDOM-NCtools/FRE2ESMF/examples/create_esmf_weights_OM4p125_d2_1x1deg.py
@@ -0,0 +1,29 @@
+import xesmf
+import xarray as xr
+import numpy as np
+
+gridspec = '/archive/Raphael.Dussin/datasets/OM4p125/OM4p125_grid_20200213_merged_bathy_unpacked'
+
+ppstatic = '/archive/Raphael.Dussin/xanadu_esm4_20190304_mom6_2019.12.16/OM4p125_JRA55do1.4_mle3d_cycle3/gfdl.ncrc4-intel16-prod/pp/ocean_annual_z_1x1deg'
+
+static_src = xr.open_dataset(f'{gridspec}/ocean_hgrid_d2.nc')
+static_src2 = xr.open_dataset(f'{gridspec}/ocean_static_d2.nc')
+
+static_src['lon'] = xr.DataArray(static_src['x'].values[1::2, 1::2].copy(), dims=('ny2','nx2'))
+static_src['lon_b'] = xr.DataArray(static_src['x'].values[0::2,0::2].copy(), dims=('ny2p1','nx2p1'))
+static_src['lat'] = xr.DataArray(static_src['y'].values[1::2,1::2].copy(), dims=('ny2','nx2'))
+static_src['lat_b'] = xr.DataArray(static_src['y'].values[0::2,0::2].copy(), dims=('ny2p1','nx2p1'))
+static_src['area2'] = xr.DataArray(static_src['area'].values[1::2,1::2].copy(), dims=('ny2','nx2'))
+static_src['mask'] = static_src2['wet'].rename({'yh':'ny2', 'xh':'nx2'})
+
+static_dst = xr.open_dataset(f'{ppstatic}/ocean_annual_z_1x1deg.static.nc')
+
+static_dst['lon_b'] = xr.DataArray(np.arange(0,360+1), dims=('lonp1'))
+static_dst['lat_b'] = xr.DataArray(np.arange(-90,90+1), dims=('latp1'))
+static_dst['mask'] = static_dst['wet'].fillna(0.)
+
+regrid = xesmf.Regridder(static_src, static_dst,
+                         method='conservative_normed',
+                         periodic=True)
+
+regrid.to_netcdf('conservative_normed_1120x1440_180x360.nc')
---
+++ FREEDOM-NCtools/FRE2ESMF/examples/example_convert_esmf2fre_1.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# NB: this needs to be run on PP/AN because of the fregrid_parallel section
+
+module load fre/bronx-18
+module load mpich2
+module load nco
+
+# some directories with grids and test data:
+static1deg=/archive/Raphael.Dussin/xanadu_esm4_20190304_mom6_2019.12.16/OM4p125_JRA55do1.4_mle3d_cycle3/gfdl.ncrc4-intel16-prod/pp/ocean_annual_z_1x1deg/ocean_annual_z_1x1deg.static.nc
+gridspec=/archive/Raphael.Dussin/datasets/OM4p125/OM4p125_grid_20200213_merged_bathy_unpacked
+annuald2=/archive/Raphael.Dussin/xanadu_esm4_20190304_mom6_2019.12.16/OM4p125_JRA55do1.4_mle3d_cycle3/gfdl.ncrc4-intel16-prod/pp/ocean_annual_z_d2/av/annual_1yr
+
+# create ESMF-style regridding weights
+python create_esmf_weights_OM4p125_d2_1x1deg.py
+
+# convert to FREGRID-style
+../esmf2fre --nx_src 1440 --ny_src 1120 --nx_dst 360 --ny_dst 180 --areafile $static1deg conservative_normed_1120x1440_180x360.nc -o regrid_weights_ESMF2FREGRID.mc
+
+# regrid test data with vanilla fregrid
+mpirun -n 4 fregrid_parallel --input_mosaic $gridspec/ocean_mosaic_d2.nc --input_file $annuald2/ocean_annual_z_d2.2018.ann.nc --scalar_field thetao --nlon 360 --nlat 180 --output_file temp_regridded_fre.nc
+
+# regrid using fregrid with the weights we converted from ESMF
+fregrid --input_mosaic $gridspec/ocean_mosaic_d2.nc --input_file $annuald2/ocean_annual_z_d2.2018.ann.nc --scalar_field thetao --nlon 360 --nlat 180 --remap_file regrid_weights_ESMF2FREGRID.nc --output_file temp_regridded.nc
+
+# check the results
+ncdiff temp_regridded.nc temp_regridded_fre.nc -o diff_esmf_vs_fregrid_algo.nc
